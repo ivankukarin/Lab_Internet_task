cd69a05fb7db77bde960fe9dded29a7aef002cfc
ivankukarin <ikukarin@yandex.ru>
2019-11-01 19:42:50 +0400

    feat: is done desktop without sticky header

---
+++ main.js
@@ -0,0 +1,65 @@
+window.onload = function() {
+
+  let sliderItems = document.querySelectorAll(".slider__item");
+  let btnPrev = document.querySelector(".slider__arrow-prev");
+  let btnNext = document.querySelector(".slider__arrow-next");
+  let currentSlide = document.querySelector(
+    ".slider-numeration__current-slide"
+  );
+  let valueOfSlides = document.querySelector(
+    ".slider-numeration__value-of-slides"
+  );
+
+  valueOfSlides.textContent = sliderItems.length;
+
+  function hasClassActive(element) {
+    return element.classList.contains("slider__item-active");
+  }
+
+  function checkSliderArrows() {
+    let arr = Array.from(sliderItems);
+    let index = arr.findIndex(hasClassActive);
+
+    if (index + 1 < arr.length) {
+      if (!btnNext.classList.contains("slider__arrow-active"))
+        btnNext.classList.add("slider__arrow-active");
+    } else {
+      btnNext.classList.remove("slider__arrow-active");
+    }
+
+    if (index != 0) {
+      if (!btnPrev.classList.contains("slider__arrow-active"))
+        btnPrev.classList.add("slider__arrow-active");
+    } else {
+      btnPrev.classList.remove("slider__arrow-active");
+    }
+  }
+
+  btnNext.addEventListener("click", () => {
+    let arr = Array.from(sliderItems);
+    let index = arr.findIndex(hasClassActive);
+
+    if (index != arr.length - 1) {
+      arr[index].classList.remove("slider__item-active");
+      arr[index + 1].classList.add("slider__item-active");
+      currentSlide.textContent++;
+    }
+
+    checkSliderArrows();
+  });
+
+  btnPrev.addEventListener("click", () => {
+    let arr = Array.from(sliderItems);
+    let index = arr.findIndex(hasClassActive);
+
+    if (index != 0) {
+      arr[index].classList.remove("slider__item-active");
+      arr[index - 1].classList.add("slider__item-active");
+      currentSlide.textContent--;
+    }
+
+    checkSliderArrows();
+  });
+
+  checkSliderArrows();
+};
